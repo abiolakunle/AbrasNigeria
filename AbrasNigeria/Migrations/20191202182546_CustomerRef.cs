ddecfde1a1c73d0bc38e1071a3be1adafa880ce9
abiolakunle <olakunle2abiola@gmail.com>
2019-12-02 20:32:46 +0100

    added customer reference to document model and applied changes to new and view document in react

---
+++ AbrasNigeria/Migrations/20191202182546_CustomerRef.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AbrasNigeria.Migrations
+{
+    public partial class CustomerRef : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CompanyRef",
+                table: "Documents",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CompanyRef",
+                table: "Documents");
+        }
+    }
+}
